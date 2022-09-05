2ae54b8eecfe7dceb85f0c48a41d7047882ad74e
Orosz Levente <oroszleventee1403@gmail.com>
2022-09-05 09:10:32 +0200

    Pablo

---
+++ src/Kecske.java
@@ -0,0 +1,9 @@
+public class Kecske {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Interfész");
+
+        Kecske pablo = new Kecske();
+        pablo.legel();
+    }
+}
+
